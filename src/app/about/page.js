31d68de8247f44c4c3552fe4e012f10e16d9c5a5
Kanan Farzali <kanan.farzali@gmail.com>
2024-02-15 12:43:11 +0100

    creating about page

---
+++ src/app/about/page.js
@@ -0,0 +1,34 @@
+import React from "react";
+
+export default function About() {
+  return (
+    <div className="max-w-6xl mx-auto p-3 space-y-4">
+      <h1 className="text-2xl font-medium text-amber-600">About</h1>
+      <p>
+        Velit officia est minim minim Lorem excepteur sunt cillum duis magna
+        occaecat enim eiusmod. Ut duis voluptate laborum officia ad eu Lorem
+        sunt minim officia aliquip ad nostrud ex. Adipisicing id labore magna
+        deserunt mollit tempor quis irure occaecat. Fugiat est amet consequat
+        officia pariatur tempor ut voluptate exercitation nisi ad est. Aute
+        nostrud officia occaecat dolore deserunt cupidatat Lorem velit elit
+        enim.
+      </p>
+      <p>
+        Consectetur consequat eu excepteur adipisicing esse incididunt labore
+        consequat dolore laborum enim. Eiusmod aliquip cupidatat dolor non
+        deserunt anim nostrud sunt. Veniam sunt duis aute excepteur ad excepteur
+        nisi laboris duis nostrud aute exercitation aliquip. Reprehenderit quis
+        aliqua proident ex adipisicing velit non ipsum elit. Ex excepteur ad
+        amet pariatur laborum occaecat est irure cupidatat esse duis tempor
+        laborum. Lorem non sit culpa consectetur eu reprehenderit id incididunt
+        ipsum elit ad cillum. Consequat culpa aliquip mollit consequat officia.
+      </p>
+      <p>
+        Ad voluptate ut dolore proident et reprehenderit non laboris excepteur.
+        Deserunt magna officia quis commodo commodo enim elit incididunt eiusmod
+        cillum. Reprehenderit eiusmod incididunt qui esse amet adipisicing eu
+        qui.
+      </p>
+    </div>
+  );
+}
